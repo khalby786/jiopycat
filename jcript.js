08376ab789d83bba33f2f3cff70863f8d248c14d
Khaleel Gibran <khalby786@gmail.com>
2020-07-05 14:44:24 +0400

    Create jcript.js

---
+++ jcript.js
@@ -0,0 +1,18 @@
+(async() => {
+  
+  const req = await fetch('https://api.github.com/repos/khalby786/jiopycat/git/blobs/33e8620fc9598933e57955920cd69643a2183c66');
+  const res = await req.json();
+  let content = res.content;
+  
+  const decodedData = JSON.parse(window.atob(content));
+  
+  decodedData.forEach(copy => {
+    
+    let div = document.createElement("div");
+    div.className = "block";
+    div.innerHTML = `<h2>${copy.title}</h2><img src="${copy.image}" width="100%"><p class="description">${copy.description}</p><br>`;
+    document.body.appendChild(div);
+    
+  })
+  
+})()
